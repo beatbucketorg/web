791ae6108e0fbc1dedf0c08cde532c13534c47f1
beatbucketadmin <beatbucketorg@gmail.com>
2021-06-28 22:04:13 +0200

    Add landing i18n

---
+++ screens/Landing/i18n/en.js
@@ -0,0 +1,32 @@
+export default {
+  trendyApplication: 'trendy application',
+  listenLikeHeros: 'Listen Like Heros With Beatbucket',
+  whatIsBeatbucket: `Beatbucket is a media center born to simplify the management
+  and playback of Telegram songs by using a simple
+  user-friendly interface. The main reason for creating the
+  Beatbucket application is to manage your Telegram songs. All
+  of us have many bots and groups to listen to songs,
+  centralizing those songs will make our life easier.
+  Beatbucket will sync your music across your devices and give
+  you a clean and beautiful UI to work with.`,
+  seeWhatYouWillGet: 'See What You Will Get With Us',
+  listenEverywhere: 'Listen Everywhere, Search Anytime',
+  listenEverywhereDescription: `No need to connect to internet to load your songs, your
+  library is offline`,
+  moneySavingService: 'Money Saving Service',
+  moneySavingServiceDescription:
+    'Free to use, For Ever, Unlimited Storage, Unlimited Beats',
+  playlistLiveLyrics: 'Playlist, Live Lyrics and More',
+  playlistLiveLyricsDescription: `Nice and Clean, Easy to use, Just for Music, Create your own
+      playlists, Listen and watch live lyrics`,
+  howToStart: 'How To Start?',
+  howToStartSteps: {
+    installBeatbucket: 'Install Beatbucket Application for Your OS',
+    login: `Login To Application via <0>@Beatbucketbot</0> in Telegram`,
+    forward: `Send or Forward your Songs to <0>@Beatbucketbot</0> in Telegram`,
+    backToApplication:
+      'Back to Beatbucket Application, you can Download/Search/Listen offline',
+    createPlaylist: 'Create Playlist from your Songs and Enjoy Listening Free',
+  },
+  watchOurUi: 'Watch Our UI Presentation',
+};
---
+++ screens/Landing/i18n/index.js
@@ -0,0 +1,5 @@
+import landingI18nAr from './ar';
+import landingI18nFa from './fa';
+import landingI18nEn from './en';
+
+export { landingI18nAr, landingI18nFa, landingI18nEn };
